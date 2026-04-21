47b1ecd613779e86521e7a776ff6b4144180e53f
xenakistheo <theoxen03@gmail.com>
2026-04-21 11:11:43 -0400

    export constraints

--- src/PCFM.jl
+++ src/PCFM.jl
@@ -13,11 +13,13 @@ include("./data.jl")
 include("./model.jl")
 include("./training.jl")
 include("./sampling.jl")
+include("./constraints.jl")
 
 # Export main functions
 export FFM
 export prepare_input, interpolate_flow
 export train_ffm!, sample_ffm, sample_pcfm
 export generate_diffusion_data
+export heat_constraints!, rd_constraints!
 
 end # module PCFM
