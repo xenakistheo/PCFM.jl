e06fdf128279546b99b14f4048f6678451c582cf
xenakistheo <theoxen03@gmail.com>
2026-04-23 16:14:57 -0400

    change time limit

--- scripts/generate_ns_data.sh
+++ scripts/generate_ns_data.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
-#SBATCH --time=12:00:00
+#SBATCH --time=06:00:00
 #SBATCH --output=logs/ns_data_%j.out
 #SBATCH --error=logs/ns_data_%j.err
 
