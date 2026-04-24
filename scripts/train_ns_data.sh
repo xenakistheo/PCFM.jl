4fe8e34f21d2abcb5f08caf13f0bdf38c11267e0
xenakistheo <theoxen03@gmail.com>
2026-04-23 22:49:38 -0400

    change jobname and lognames

--- scripts/train_ns_data.sh
+++ scripts/train_ns_data.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=pcfm_ns_data
+#SBATCH --job-name=pcfm_ns_train
 #SBATCH --partition=mit_normal_gpu
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=128G
 #SBATCH --time=06:00:00
-#SBATCH --output=logs/ns_data_%j.out
-#SBATCH --error=logs/ns_data_%j.err
+#SBATCH --output=logs/ns_train_%j.out
+#SBATCH --error=logs/ns_train_%j.err
 
 set -euo pipefail
 
