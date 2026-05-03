935a7453041f7feeb16b55dbd409f4d5f8fab31e
xenakistheo <theoxen03@gmail.com>
2026-05-02 21:32:15 -0400

    add cuDNN and CUDA

--- src/PCFM.jl
+++ src/PCFM.jl
@@ -2,11 +2,14 @@ module PCFM
 
 using NeuralOperators
 using Lux
+using JLD2, Functors
+using cuDNN
+using CUDA
 using Random
 using Optimisers
 using Reactant
 using KernelAbstractions
-using MadNLP, MadNLPGPU
+
 using JuMP
 using HDF5
 
