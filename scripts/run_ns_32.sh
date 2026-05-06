fa70c3907b36ff42fa348eedd3a2d8b937929ae6
xenakistheo <theoxen03@gmail.com>
2026-05-05 22:57:15 -0400

    change time limit

--- scripts/run_ns_32.sh
+++ scripts/run_ns_32.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
-#SBATCH --time=12:00:00
+#SBATCH --time=06:00:00
 #SBATCH --output=logs/ns_32_%j.out
 #SBATCH --error=logs/ns_32_%j.err
 
