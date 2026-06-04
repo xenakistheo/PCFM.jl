9f796a53046e04b6ec571c2f0886dd33bbdc1046
xenakistheo <theoxen03@gmail.com>
2026-06-04 15:19:50 -0400

    add alainas imports

--- src/PCFM.jl
+++ src/PCFM.jl
@@ -14,6 +14,13 @@ using MadNLP, MadNLPGPU, ExaModels
 using JuMP
 using HDF5
 
+using FFTW 
+using Optimization
+using OptimizationOptimJL
+
+using ForwardDiff
+using ADTypes
+using DifferentiationInterface
 # Make training API origin explicit for downstream includes.
 const Training = Lux.Training
 
@@ -21,6 +28,7 @@ const Training = Lux.Training
 include("./data.jl")
 include("./model.jl")
 include("./training.jl")
+include("./projection.jl")
 include("./sampling.jl")
 include("./constraints.jl")
 
@@ -33,5 +41,35 @@ export generate_diffusion_data
 export load_burgers_batch, load_rd_batch, load_ns_batch
 # Constraints
 export heat_constraints!, rd_constraints!, rd_constraints_2!, burgers_constraints_BC_Mass!, burgers_constraints_IC_Mass_Flux!, ns_constraints!
+
+
+
+# Alaina's
+export NoOpSolver
+export AbstractProjectionSolver
+export AnalyticICProjectionSolver
+export AnalyticEnergyProjectionSolver
+export AnalyticICEnergyProjectionSolver
+export AnalyticMassProjectionSolver
+export AnalyticICMassProjectionSolver
+export PenaltyLBFGSEnergyProjectionSolver
+export PenaltyLBFGSMassProjectionSolver
+export IPEnergyProjectionSolver
+export IPMassProjectionSolver
+export RDSolver
+export NSVorticityAnalyticSolver
+export NSVorticityLBFGSSolver
+export RDIPNewtonSolver
+export NSVorticityIPNewtonSolver
+export BurgersBCMassSolver, BurgersBCMassIPSolver
+export BurgersICFluxSolver, BurgersICFluxIPSolver
+
+export solve_projection, make_constraint_data
+
+export prepare_input, interpolate_flow
+export train_ffm!, sample_ffm, sample_pcfm #_final
+export generate_diffusion_data
+export get_array_layout, get_slice, n_spatial, set_slice!
+
 
 end # module PCFM
