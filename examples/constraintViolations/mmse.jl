89808e38d30e1c26aa8137b3afb7b16943de839c
xenakistheo <theoxen03@gmail.com>
2026-06-06 17:41:09 -0400

    in progress

--- examples/constraintViolations/mmse.jl
+++ examples/constraintViolations/mmse.jl
@@ -12,9 +12,18 @@ typeof(data_path)
 
 heat_data = JLD2.load(joinpath(data_path, "samples_heat.jld2"))
 
-heat_data["results"]
+heat_data
 
-samples_exa_gpu     = heat_data["samples_exa_gpu"]
-samples_exa_cpu     = data["samples_exa_cpu"]
-samples_jump_madnlp = data["samples_jump_madnlp"]
+samples_exa_gpu     = heat_data["samples_exa_gpu"][:,:,1,:]
+samples_exa_cpu     = heat_data["samples_exa_cpu"][:,:,1,:]
+samples_jump_madnlp = heat_data["samples_jump_madnlp"][:,:,1,:]
 
+
+analytic_solution = heat_data["u_analytic"][:,:,1,:]
+
+mean_arr = dropdims(mean(samples_exa_gpu, dims=3), dims=3)  # 100x100
+std_arr  = dropdims(std(samples_exa_gpu, dims=3), dims=3)   # 100x100
+
+std_analytic = dropdims(std(analytic_solution, dims=3), dims=3)  # 100x100
+
+analytic_solution[:,:,2]
